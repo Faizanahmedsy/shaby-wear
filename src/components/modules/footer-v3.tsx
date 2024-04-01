7b9e13f64eb75f55efcac88eb001fbd7ee65ead7
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-04-01 23:05:21 +0530

    feat: finished

--- src/components/modules/footer-v3.tsx
+++ src/components/modules/footer-v3.tsx
@@ -3,27 +3,21 @@ import { Button } from "../ui/button";
 
 const FooterV3 = () => {
   return (
-    <footer className="py-12">
+    <footer className="py-12 bg-brand-primary text-violet-200">
       <div className="container mx-auto flex flex-col lg:flex-row justify-between ">
         <div className="flex flex-col items-center lg:items-start mb-6 lg:mb-0">
-          <h2 className="text-3xl font-bold text-brand-primary mb-2">
-            Shaby Wear
-          </h2>
+          <h2 className="text-3xl font-bold text-white mb-2">Shaby Wear</h2>
           <p className="text-sm">Building a Sustainable Future</p>
         </div>
 
         <div className="flex flex-col items-center lg:items-start mb-6 lg:mb-0">
-          <h3 className="text-lg font-bold text-brand-primary mb-3">
-            Contact Us
-          </h3>
+          <h3 className="text-lg font-bold text-white mb-3">Contact Us</h3>
           <p className="text-sm">+91 9876543210</p>
           <p className="text-sm">info@roadcraft.com</p>
         </div>
 
         <div className="flex flex-col items-center lg:items-start">
-          <h3 className="text-lg font-bold text-brand-secondary mb-3">
-            Quick Links
-          </h3>
+          <h3 className="text-lg font-bold text-white mb-3">Quick Links</h3>
           <a href="/about" className="text-sm hover:text-brand-secondary mb-2">
             About Us
           </a>
@@ -55,7 +49,7 @@ const FooterV3 = () => {
       </div>
 
       <div className="mt-8 text-center">
-        <p className="text-sm text-gray-500">
+        <p className="text-sm text-white">
           Get in touch with us for any queries or feedback:
         </p>
         <div className="flex items-center justify-center mt-4">
@@ -67,11 +61,11 @@ const FooterV3 = () => {
           {/* <button className="bg-teal-800 text-white py-2 px-4 rounded-md hover:bg-teal-600 focus:outline-none">
             Submit
           </button> */}
-          <Button>Submit</Button>
+          <Button variant={"secondary"}>Submit</Button>
         </div>
       </div>
 
-      <div className="mt-8 text-center text-gray-500">
+      <div className="mt-8 text-center text-white">
         &copy; {new Date().getFullYear()} Shaby Wear. All rights reserved.
       </div>
     </footer>
